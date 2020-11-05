22490223791909ba1a28092e67f918cc1e3c9912
jjuny768 <73977108+jjuny768@users.noreply.github.com>
2020-11-05 12:06:38 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("쭈니봇")
+    await client.change_presence(status=discord.Status.online)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("안녕"):
+        await message.channel.send("HI")
+    if message.content.startswith("뭐해"):
+        await message.channel.send("게임해")
+        if message.content.startswith("공부"):
+            await message.channel.send("알겠어용")
+
+
+
+
+client.run("NzczNzE0ODI2NzY4ODA5OTg1.X6NQFQ.nsuN3PEFB0AM11w2XIIAbJHQ5kw")
